f69eaf035021e50c22607c33b9fd693a8c92b14d
IDestery <t1m0shenko@yandex.ru>
2021-09-10 12:21:28 +0300

    fcommit

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 ﻿#include <openssl/ssl.h>
-#include <winsock2.h>
-#include <Ws2tcpip.h>
-
-#pragma comment(lib, "ws2_32.lib")
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <string.h>
 
 #define HOST "github.com"
 #define DIR "/IDesteny"
@@ -10,74 +10,68 @@
 /* 128 KB */
 #define SRES 128 * 1024
 
-INT WINAPI main(VOID)
+int main(void)
 {
-	WSADATA wsaData;
-	INT iResultWSAStartup = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (iResultWSAStartup != EXIT_SUCCESS) return EXIT_FAILURE;
+	const char *port = "443";
+	struct addrinfo *pAddr = NULL;
 
-	LPCSTR port = "443";
-	PADDRINFOA pAddr = NULL;
-	INT iResultGetAddrInfo = getaddrinfo(HOST, port, NULL, &pAddr);
-	if (iResultGetAddrInfo != EXIT_SUCCESS) return EXIT_FAILURE;
+	int iResultGetAddrInfo = getaddrinfo(HOST, port, NULL, &pAddr);
+	if (iResultGetAddrInfo != 0) return 1;
 
-	SOCKET sock = socket(pAddr->ai_family, pAddr->ai_socktype, pAddr->ai_protocol);
-	if (sock == INVALID_SOCKET) return EXIT_FAILURE;
+	int sock = socket(pAddr->ai_family, pAddr->ai_socktype, pAddr->ai_protocol);
+	if (sock == -1) return 1;
 
-	INT iResultConnet = connect(sock, pAddr->ai_addr, (INT)pAddr->ai_addrlen);
-	if (iResultConnet != EXIT_SUCCESS) return EXIT_FAILURE;
+	int iResultConnet = connect(sock, pAddr->ai_addr, pAddr->ai_addrlen);
+	if (iResultConnet != 0) return 1;
 
 	SSL_library_init();
 	SSL *ssl = SSL_new(SSL_CTX_new(SSLv23_method()));
-	if (ssl == NULL) return EXIT_FAILURE;
+	if (ssl == NULL) return 1;
 
-	INT iResultSSL_set_fd = SSL_set_fd(ssl, (INT)sock);
-	if (iResultSSL_set_fd == FALSE) return EXIT_FAILURE;
+	int iResultSSL_set_fd = SSL_set_fd(ssl, sock);
+	if (iResultSSL_set_fd == 0) return 1;
 
-	INT iResultSSL_connect = SSL_connect(ssl);
-	if (iResultSSL_connect != TRUE) return EXIT_FAILURE;
+	int iResultSSL_connect = SSL_connect(ssl);
+	if (iResultSSL_connect != 1) return 1;
 
-	LPCSTR template_headres = "GET %s HTTP/1.1\r\nHOST:%s\r\nConnection:close\r\n\r\n";
-	SIZE_T headers_len = strlen(template_headres) + strlen(HOST) + strlen(DIR);
-	LPSTR headres = malloc(headers_len);
-	if (headres == NULL) return EXIT_FAILURE;
+	const char *template_headers = "GET %s HTTP/1.1\r\nHOST:%s\r\nConnection:close\r\n\r\n";
+	size_t headers_len = strlen(template_headers) + strlen(HOST) + strlen(DIR);
+	char *headers = malloc(headers_len);
+	if (headers == NULL) return 1;
 
-	INT iResultSprintf_s = sprintf_s(headres, headers_len, template_headres, DIR, HOST);
-	if (iResultSprintf_s == EOF) return EXIT_FAILURE;
+	int iResultSprintf_s = snprintf(headers, headers_len, template_headers, DIR, HOST);
+	if (iResultSprintf_s == -1) return 1;
 
-	INT iResultSSL_write = SSL_write(ssl, headres, (INT)headers_len);
-	if (iResultSSL_write <= EXIT_SUCCESS) return EXIT_FAILURE;
+	int iResultSSL_write = SSL_write(ssl, headers, headers_len);
+	if (iResultSSL_write <= 0) return 1;
 
-	LPSTR res = calloc(SRES, sizeof(CHAR));
-	if (res == NULL) return EXIT_FAILURE;
+	char *res = calloc(SRES, sizeof(char));
+	if (res == NULL) return 1;
 
-	INT iResultSSL_read = 0;
-	INT countReaded = 0;
+	int iResultSSL_read = 0;
+	int countReaded = 0;
 	do
 	{
 		iResultSSL_read = SSL_read(ssl, res + countReaded, SRES - countReaded);
 		countReaded += iResultSSL_read;
-	} while (iResultSSL_read > FALSE && countReaded < SRES);
+	} while (iResultSSL_read > 0 && countReaded < SRES);
 
-	INT iResultSSL_shutdown = SSL_shutdown(ssl);
-	if (iResultSSL_shutdown < EXIT_SUCCESS) return EXIT_FAILURE;
+	int iResultSSL_shutdown = SSL_shutdown(ssl);
+	if (iResultSSL_shutdown < 0) return 1;
 
-	INT iResultSSL_clear = SSL_clear(ssl);
-	if (iResultSSL_clear == FALSE) return EXIT_FAILURE;
+	int iResultSSL_clear = SSL_clear(ssl);
+	if (iResultSSL_clear == 0) return 1;
 
-	free(headres);
+	free(headers);
 	free(res);
 	SSL_free(ssl);
 	freeaddrinfo(pAddr);
 
-	INT iResultShutdown = shutdown(sock, SD_BOTH);
-	if (iResultShutdown != EXIT_SUCCESS) return EXIT_FAILURE;
+	int iResultShutdown = shutdown(sock, 2);
+	if (iResultShutdown != 0) return 1;
 
-	INT iResultClosesocket = closesocket(sock);
-	if (iResultClosesocket != EXIT_SUCCESS) return EXIT_FAILURE;
+	int iResultClose = close(sock);
+	if (iResultClose == -1) return 1;
 
-	INT iResultWSACleanup = WSACleanup();
-	if (iResultWSACleanup != EXIT_SUCCESS) return EXIT_FAILURE;
-
-	return EXIT_SUCCESS;
+	return 0;
 }
